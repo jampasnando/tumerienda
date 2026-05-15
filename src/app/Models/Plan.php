77d1ccd6db0d9102e72b4a764d2edc54271a9d3c
jampasnando <jampasnando@gmail.com>
2026-05-15 05:58:19 -0400

    fix(Plan) anadidod table=plans

--- src/app/Models/Plan.php
+++ src/app/Models/Plan.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Plan extends Model
 {
     use SoftDeletes;
+    protected $table = 'plans';
     protected $fillable = [
         'nombre',
         'estado',
