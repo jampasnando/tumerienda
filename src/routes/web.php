109389f7db238774bd5291c272e9fbdd8c4860cd
jampasnando <jampasnando@gmail.com>
2026-07-12 23:12:56 -0400

    feat(.env y web.php anadid SMTP y ruta de proeba

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,4 +10,28 @@ Route::get('/', function () {
 
 Route::get('/politica-privacidad', function () {
     return view('politica_privacidad');
+});
+Route::get('/mail-test', function () {
+
+    try {
+
+        Mail::raw('Este es un correo de prueba enviado desde Brevo.', function ($message) {
+            $message->to('jampasnando@gmail.com')
+                    ->subject('Prueba de correo TuMerienda');
+        });
+
+        return response()->json([
+            'ok' => true,
+            'mensaje' => 'Correo enviado correctamente.'
+        ]);
+
+    } catch (\Exception $e) {
+
+        return response()->json([
+            'ok' => false,
+            'error' => $e->getMessage()
+        ],500);
+
+    }
+
 });
