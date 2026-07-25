a91297110f628f8e9468299ae2d5989a0ecef18b
jampasnando <jampasnando@gmail.com>
2026-07-25 16:24:22 -0400

    en AdminPanvelProvider globalsearcha  a false

--- src/app/Providers/Filament/AdminPanelProvider.php
+++ src/app/Providers/Filament/AdminPanelProvider.php
@@ -31,6 +31,7 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login()
             ->profile()
+            ->globalSearch(false)
             ->colors([
                 'primary' => Color::Amber,
             ])
